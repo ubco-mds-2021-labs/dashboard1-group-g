9129a410e29ea0074a4ee316981eefe96ab60c98
SaraAnnHall <sara.hall@shaw.ca>
2022-03-09 19:01:33 -0800

    Add year slider

--- src/timecount.R
+++ src/timecount.R
@@ -7,6 +7,8 @@ library(dashBootstrapComponents)
 library(plotly)
 
 data <- read.csv('https://github.com/ubco-mds-2021-labs/dashboard1-group-g/raw/main/data/clean_spotify.csv', sep = '\t')
+year_list <- as.list(as.character(seq(1957,2020, by = 3)))
+names(year_list) <- as.character(seq(1957,2020, by = 3))
 
 count_vs_year <- function(data){
   plot <- ggplot(data, aes(x = Year, color = Playlist.Genre)) + 
@@ -14,7 +16,6 @@ count_vs_year <- function(data){
     theme_classic() + 
     labs(x = "Album Release Year", y = "Number of Songs Released", color = "Genre")
 }
-
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
@@ -31,22 +32,39 @@ app$layout(
                        list(label = "R&B", value = "R&B"),
                        list(label = "Edm", value = "Edm")
                        ),
-        value = unique(df$Playlist.Genre), 
+        value = unique(data$Playlist.Genre), 
         multi = TRUE
+      ),
+      htmlDiv(
+        list(
+          htmlLabel('Album Release Year'),
+          dccRangeSlider(
+            id = 'year-widget',
+            min = 1957,
+            max = 2020,
+            marks = year_list,
+            value = list(1957,2020)
+          )
+          
+        )
       )
     )
   )
 )
 
-app$callback(
+#Callback for the time count plot.
+app |> add_callback(
   output('timecountplot', 'figure'),
-  list(input('genre-widget', 'value')),
-  function(genres) {
-    print(genres)
-    newData <- data |> filter(Playlist.Genre %in% genres)
+  list(input('genre-widget', 'value'), 
+       input('year-widget', 'value')),
+  function(genres, years) {
+    newData <- data |> filter(Playlist.Genre %in% genres, 
+                              Year >= as.integer(years[[1]]), 
+                              Year <= as.integer(years[[2]]))
     p <- count_vs_year(newData)
     ggplotly(p)
   }
 )
+
 
 app$run_server(debug = T)
